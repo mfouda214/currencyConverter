e5726c2ea6512bbb2c85f1f00acc5c44e43af488
mfouda214 <m.fouda@robone.net>
2018-04-06 21:50:42 +0100

    Add ShowReason Control class

--- CurrencyConverter/ShowViewController.swift
+++ CurrencyConverter/ShowViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class ShowViewController: UIViewController {
 
+
+    let fileManager = FileManager.default
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        
+        
+        
+        ShowText.text = FileSaved()
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +29,27 @@ class ShowViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBOutlet weak var ShowText: UITextView!
+    
+    func FileSaved() -> String{
+        do {
+            let documents = try fileManager.url(for: FileManager.SearchPathDirectory.documentDirectory, in: FileManager.SearchPathDomainMask.userDomainMask, appropriateFor: nil, create: false)
+            
+            print(documents)
+            let url = URL(string: "Reason.txt", relativeTo: documents)
+    
+            if let url = url {
+                
+                let textFromFile = try String(contentsOf: url)
+                let text = textFromFile
+                
+                return text
+            }
+        } catch {
+            print("Error getting path")
+        }
+        return "nil"
+    }
 
     /*
     // MARK: - Navigation
